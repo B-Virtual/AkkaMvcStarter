00170e32ee7d7421964ddc14406a62f381238267
Bjorn Bailleul <bjorn@b-virtual.com>
2018-05-18 10:57:54 +0200

    Added status controller

---
+++ FaTi/Controllers/StatusController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BVirtual.FaTi.Controllers
+{
+    [Route("/api/status")]
+    [Route("/api/v1/status")]
+    public class StatusController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+
+                var result = new
+                {
+                    Result = "SUCCESS",
+                };
+
+                return new JsonResult(result);
+            }
+            catch (Exception e)
+            {
+                var result = new
+                {
+                    Result = "FAILED",
+                    Description = e
+                };
+
+                return new JsonResult(result);
+            }
+        }
+
+    }
+}
